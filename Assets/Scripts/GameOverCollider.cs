4e0dccdc28eb45678e7141a80c8dc8c254dada88
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2024-12-08 05:42:22 +0300

    Game Over Collider and Water Collider Added

---
+++ Assets/Scripts/GameOverCollider.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class GameOverCollider : MonoBehaviour
+{
+    [SerializeField] private bool isDog;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cat")) GameOverManager.Instance.GameOver(isDog).Forget();
+    }
+}
